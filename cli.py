acbe75bfbb710dac18eace5393de51f0cb26b2ef
Michael Allen Jr <mallen3699@gmail.com>
2022-03-11 00:43:34 -0600

    Updated cli to add the ability to run tests

--- cli.py
+++ cli.py
@@ -1,4 +1,6 @@
 from bmi import getBmi, getCategory
+import sys
+import unittest
 
 def cli():
     print("Welcome to the BMI calculator!")
@@ -11,5 +13,14 @@ def cli():
     category = getCategory(bmi)
     print(f"Your BMI is {bmi}. According to the CDC, you are {category}.")
 
-if __name__ == "__main__":
+def main():
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "test":
+            testsuite = unittest.TestLoader().loadTestsFromName('bmi')
+            unittest.TextTestRunner(verbosity=1).run(testsuite)
+            return
+
     cli()
+
+if __name__ == "__main__":
+    main()
