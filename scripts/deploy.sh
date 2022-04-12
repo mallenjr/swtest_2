d1b644febb8d8d38612b378e5840f702ec5d7922
Mike Allen <mike@mmjr.it>
2022-04-12 18:58:09 -0500

    Updated depoyment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-curl -s -X POST http://qa2.mmjr.it:5000/deploy \
-    -H 'Content-Type: application/json' \
-    -d "{\"new_tag\": \"latest\"}"
+curl -s -X POST https://qa2.mmjr.it/deploy -H 'Content-Type: application/json' -d "{\"new_tag\": \"latest\"}"
