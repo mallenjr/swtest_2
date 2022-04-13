e72cc51fb427e53b3726a8a38229b5f6c409fe07
Mike Allen <mike@mmjr.it>
2022-04-12 19:04:07 -0500

    Bugfix

--- process.py
+++ process.py
@@ -19,8 +19,8 @@ def get_image_tag(image):
 def bootstrap():
     bmicalc_tag = get_image_tag("bmicalc")
     try:
-        os.system("docker rm bmicalc")
         os.system("docker stop bmicalc")
+        os.system("docker rm bmicalc")
         os.system("""docker pull majrlzr/bmicalc:latest""")
         os.system("""docker run -d --name bmicalc -p 4000:4000 majrlzr/bmicalc:latest""")
     except Exception as e:
@@ -37,8 +37,8 @@ def update_environment(image, tag):
 def handle_deploy(new_tag):
     update_environment("bmicalc", new_tag)
     try:
-        os.system("docker rm bmicalc")
         os.system("docker stop bmicalc")
+        os.system("docker rm bmicalc")
         os.system("""docker pull majrlzr/bmicalc:latest""")
         os.system("""docker run -d --name bmicalc -p 4000:4000 majrlzr/bmicalc:latest""")
     except Exception as e:
